a7339d31b5d9f4d746c2849adfdb5ba101ac2887
T0ny6969 <togoruiz08@hotmail.com>
2024-01-17 18:30:27 -0600

    Creación de funcion para detección de numeros

--- NumberDetector.py
+++ NumberDetector.py
@@ -19,29 +19,41 @@ pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tessera
 
 "First we load the desired image"
 imagen = cv2.imread('numeros.png')
+"""With this we can now the widht and heigh of the image
+        rows, cols, _ = imagen.shape
+        print(f'Los pixeles de filas son {rows} y de las columnas son {cols}')
+After running this code, we now know that the image is 675 x 1200 pxls """
 
 """This will show in  window the image that we choose"""
 cv2.imshow('imagen', imagen)
 cv2.waitKey(0)
 
-"""This will allow us to transform the image to one in a gray scale"""
-imagen_gris = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-cv2.imshow('imagen gris', imagen_gris)
-cv2.waitKey(0)
-
-"""Thresholding == 'umbral' o 'umbralización' """
-"""The function threshold returns a tuple, and the funtion 'cv2.imshow' only shows 'arrays', therefore this is solved 
-by adding the '[1]' at the end of the method 'threshold'. In case further doubts show up, print the variable type"""
-imagen_procesada = cv2.threshold(imagen_gris, 80, 255, cv2.THRESH_BINARY)[1]
-cv2.imshow('imagen procesada', imagen_procesada)
-cv2.waitKey(0)
-
-option1 = '-c tessedit_char_whitelist=0123456789 --psm 11 --oem 0'
-option2 = 'tesseract input_file output_file --oem 0 -c tessedit_char_whitelist=0123456789'
-texto = pytesseract.image_to_string(imagen_procesada, lang='eng', config='--psm 8')
-print(texto)
+imagen_cortada = imagen[0: 300, 0:600]
+cv2.imshow('Imagen recortada', imagen_cortada)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+
+def identify_number(img):
+    """This will allow us to transform the choosen image to one in a gray scale"""
+    imagen_gris = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('imagen gris', imagen_gris)
+    cv2.waitKey(0)
+
+    """Thresholding == 'umbral' o 'umbralización'
+    The function threshold returns a tuple, and the funtion 'cv2.imshow' only shows 'arrays', therefore this is 
+    solved by adding the '[1]' at the end of the method 'threshold'. In case further doubts show up, print the variable 
+    type"""
+    imagen_procesada = cv2.threshold(imagen_gris, 80, 255, cv2.THRESH_BINARY)[1]
+    cv2.imshow('imagen procesada', imagen_procesada)
+    cv2.waitKey(0)
+
+    """With this section we use pytesseract to analyze the processed image and detect a number. We need to check 
+    further in the 'config' section to get a better result accuracy"""
+    option1 = '-c tessedit_char_whitelist=0123456789 --psm 11 --oem 0'
+    option2 = 'tesseract input_file output_file --oem 0 -c tessedit_char_whitelist=0123456789'
+    texto = pytesseract.image_to_string(imagen_procesada, lang='eng', config='--psm 8')
+    print(texto)
 
 
