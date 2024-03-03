db0663f4031a39516e9aa779a690941aa63660e9
T0ny6969 <togoruiz08@hotmail.com>
2024-03-03 15:09:17 -0600

    Added functionality and rearranged folders

--- iot_platform/medicalIOT/IoTCloud/admin.py
+++ iot_platform/medicalIOT/IoTCloud/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Hospital
+from .models import Hospital, Medico, Paciente
 
 
 # Register your models here.
@@ -7,4 +7,14 @@ class HospitalAdmin(admin.ModelAdmin):
     readonly_fields = ('created', 'updated')
 
 
+class MedicoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created', 'updated')
+
+
+class PacienteAdmin(admin.ModelAdmin):
+    readonly_fields = ('ingreso', 'updated')
+
+
 admin.site.register(Hospital, HospitalAdmin)
+admin.site.register(Medico, MedicoAdmin)
+admin.site.register(Paciente, PacienteAdmin)
--- iot_platform/medicalIOT/IoTCloud/models.py
+++ iot_platform/medicalIOT/IoTCloud/models.py
@@ -1,4 +1,3 @@
-from datetime import datetime
 from django.db import models
 
 """When creating or updating a model for an app, 2 commands are esential here:
@@ -6,7 +5,19 @@ from django.db import models
    python manage.py migrate 'app name'  
    
    this commands will modify the DataBase by updating the existing models, or creating the new models, if this 
-   commands are not executed, the changes will make no effect in the server and the DataBase """
+   commands are not executed, the changes will make no effect in the server and the DataBase 
+   
+   ERROR warnings: 
+   If the database has already information, and we modify the models, we will probably see the following error:
+   
+   'It is impossible to add a non-nullable field 'class example name' to feature without specifying a default. This is 
+   because the database needs something to populate existing rows.'
+   
+   In this case we have 2 options, set a default value, default=''
+   Or
+   Delete the migration data files, in the 'migrations' file of the app, (DO NOT DELETE THE __init__.py file). However,
+   this solution will erase our current data, therefore we need to be cautious about this.
+   """
 
 
 # Create your models here.
@@ -32,11 +43,42 @@ class Hospital(models.Model):
 
 
 class Medico(models.Model):
-    Nombre= models.CharField(max_length=30)
-    Apellido= models.CharField(max_length=30)
+    nombre = models.CharField(max_length=40, verbose_name='Nombre')
+    apellidopaterno = models.CharField(max_length=30, verbose_name='Apellido paterno')
+    apellidomaterno = models.CharField(max_length=30, verbose_name='Apellido materno')
+    especialidad = models.CharField(max_length=40, verbose_name='Especialidad médica')
+    # With this, the register saves the date when its created. By default, Django hides this to avoid modifications
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    # With this, the register saves the date when its updated. By default, Django hides this to avoid modifications
+    updated = models.DateTimeField(auto_now=True, verbose_name="Ultima modificación")
+
+    # This class is used to change the public name of the model in the 'admin' section
+    class Meta:
+        verbose_name = "Doctor"
+        verbose_name_plural = "Doctores"
+        ordering = ["-created"]  # puting a '-' indicates to go from newer to older
+
+    # With this function we request to show the title as the public name, instead of a generic project name
+    def __str__(self):
+        return self.nombre
 
 
 class Paciente(models.Model):
-    Nombre= models.CharField(max_length=30)
-    Apellido= models.CharField(max_length=30)
-    Ingreso= models.DateField(datetime)
+    nombre = models.CharField(max_length=40, verbose_name='Nombre')
+    apellidopaterno = models.CharField(max_length=30, verbose_name='Apellido paterno')
+    apellidomaterno = models.CharField(max_length=30, verbose_name='Apellido materno')
+    # With this, the register saves the date when its created. By default, Django hides this to avoid modifications
+    ingreso = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de ingreso")
+    # With this, the register saves the date when its updated. By default, Django hides this to avoid modifications
+    updated = models.DateTimeField(auto_now=True, verbose_name="Ultima modificación")
+
+    # This class is used to change the public name of the model in the 'admin' section
+    class Meta:
+        verbose_name = "Paciente"
+        verbose_name_plural = "Pacientes"
+        ordering = ["-ingreso"]  # puting a '-' indicates to go from newer to older
+
+    # With this function we request to show the title as the public name, instead of a generic project name
+    def __str__(self):
+        return self.nombre
+
