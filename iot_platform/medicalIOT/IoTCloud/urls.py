27c2de6eb77efabfee5c34832b63d96384ac2e26
T0ny6969 <togoruiz08@hotmail.com>
2024-03-09 20:17:32 -0600

    V2.5 lista, base de datos agregada, funcionalidad de agregar nuevos pacientes lista

--- iot_platform/medicalIOT/IoTCloud/urls.py
+++ iot_platform/medicalIOT/IoTCloud/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import PacientesForm, Pacientes
 
 urlpatterns = [
     path('', views.home, name="Home"),
@@ -7,8 +8,9 @@ urlpatterns = [
     path('dashboard/', views.dashboard, name='Dashboard'),
     path('get_chart/', views.get_chart, name='get_chart'),
     path('hospitales/', views.hospitales, name='Hospitales'),
-    path('pacientes/', views.pacientes, name='Pacientes'),
+    path('pacientes/', Pacientes.as_view(), name='Pacientes'),
     path('configuracion/', views.configuracion, name='Configuracion'),
     path('aviso-privacidad/', views.aviso_privacidad, name='AvisoPrivacidad'),
     path('about-us', views.about_us, name='Historia'),
+    path('nuevo-paciente/', PacientesForm.as_view(), name='PacienteNuevo')
 ]
--- iot_platform/medicalIOT/IoTCloud/views.py
+++ iot_platform/medicalIOT/IoTCloud/views.py
@@ -1,5 +1,10 @@
 from django.http.response import JsonResponse
 from django.shortcuts import render
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.urls import reverse, reverse_lazy
+from django.db.models import Count
+from .models import Paciente
 from random import randrange
 
 
@@ -20,10 +25,6 @@ def hospitales(request):
     return render(request, "iot_temp/hospitales.html")
 
 
-def pacientes(request):
-    return render(request, "iot_temp/pacientes.html")
-
-
 def configuracion(request):
     return render(request, "iot_temp/configuracion.html")
 
@@ -36,52 +37,74 @@ def about_us(request):
     return render(request, "iot_temp/IoTHistory.html")
 
 
+class Pacientes(ListView):
+
+    template_name = 'iot_temp/pacientes.html'
+    model = Paciente
+
+
+class PacientesForm(CreateView):
+
+    # With this we instruct which HTML the class should render/create
+    template_name = 'iot_temp/pacientes_form.html'
+
+    # First we need to import the model 'Paciente' so we can access to the database
+    model = Paciente
+    fields = ['nombre', 'apellidopaterno', 'apellidomaterno']
+
+    success_url = reverse_lazy('Pacientes')
+
+
 # Estructura de datos Json para construir gráfico
 def get_chart(request):
-    
     # colors = ['blue', 'black', 'yellow', 'orange', 'magenta', 'purple', 'red', 'purple']
     # random_color=colors[randrange(0, len(colors-1))]
 
     serie = []
     counter = 0
 
-    while(counter<24):
-        serie.append(randrange(60,100))
+    while counter < 24:
+        serie.append(randrange(60, 100))
         counter += 1
 
     chart = {
         'tooltip': {
-            'show':True,
-            'trigger':"axis",
-            'triggerOn':"mousemove|click"
+            'show': True,
+            'trigger': "axis",
+            'triggerOn': "mousemove|click"
         },
-        'xAxis':[
+
+        'xAxis': [
             {
-                #category puede ser texto
-                'type':"category",
-                'data':["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00","13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"]
+                # category puede ser texto
+                'type': "category",
+                'data': ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00",
+                         "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00",
+                         "20:00", "21:00", "22:00", "23:00"]
 
             }
-        ], 
-        'yAxis':[
+        ],
+
+        'yAxis': [
             {
-                #value = números
-                'type':"value"
+                # value = números
+                'type': "value"
             }
         ],
-        'series':[
+
+        'series': [
             {
-                'data':serie,
-                'type':"line",
-                'smooth':"true",
-                #'itemStyle':{
-                    #'color': random_color
-                #},
-                #'lineStyle':{
-                    #'color': random_color
-                #}
+                'data': serie,
+                'type': "line",
+                'smooth': "true",
+                # 'itemStyle':{
+                # 'color': random_color
+                # },
+                # 'lineStyle':{
+                # 'color': random_color
+                # }
             }
         ]
-    
+
     }
     return JsonResponse(chart)
