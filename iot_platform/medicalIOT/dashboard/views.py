395db975735da8c681f600d3cf149d8ca8e9fda0
T0ny6969 <togoruiz08@hotmail.com>
2024-03-28 20:28:37 -0600

    v4.1 lista, datos de pulsioximetro visibles

--- iot_platform/medicalIOT/dashboard/views.py
+++ iot_platform/medicalIOT/dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from random import randrange
 # from iot_platform.medicalIOT.api_endpoint.models import MedicalMonitor
 from django.http.response import JsonResponse
-from api_endpoint.models import MedicalMonitor
+from api_endpoint.models import MedicalMonitor, BeatsPerMinute
 from django.views.generic import DetailView
 
 
@@ -11,7 +11,8 @@ def dashboard(request):
     """We can retrieve the firs() and last() object of a mode, but since our model is already sorted by the latest
        created (see our models.py) then in this case we need to retrieve the first() so we have the most recent object"""
     signos = MedicalMonitor.objects.first()
-    return render(request, "dashboard/dashboard.html", {"signos": signos})
+    beats = BeatsPerMinute.objects.first()
+    return render(request, "dashboard/dashboard.html", {"signos": signos, "beats": beats})
 
 
 # Estructura de datos Json para construir gráfico
